2ba8b329ce582e525422f471f1a6eebcb664ba0c
hoa.phan-van <hoa.phan-van@banvien.com.vn>
2021-08-01 12:45:18 +0700

    fix path

--- src/components/CurvedBottomBar/components/navigator/path.ts
+++ src/components/CurvedBottomBar/components/navigator/path.ts
@@ -2,74 +2,93 @@ import * as shape from 'd3-shape';
 
 export const getPath = (width: number, height: number, centerWidth: number, borderTopLeftRight: boolean = false) => {
   const circleWidth = centerWidth + 16;
-  const left = shape
-    .line()
-    .x((d) => d.x)
-    .y((d) => d.y)([
+
+  const line = shape
+  .line()
+  .x((d) => d.x)
+  .y((d) => d.y)([
+    { x: (width - circleWidth) / 2 + circleWidth + 20, y: 0 },
+    { x: width, y: 0 },
+    { x: width, y: height },
+    { x: 0, y: height },
     { x: 0, y: 0 },
-    // { x: width, y: 0 },
+    { x: (width - circleWidth) / 2 - 20, y: 0 },
   ]);
 
-  const borderTopLeft = borderTopLeftRight ? [
-    { x: 0 - 10, y: 0 }, // end left
-    { x: 0 - 10, y: height },
-    { x: 0, y: height },
-
-    { x: 0, y: 20 }, // border left
-    { x: 0 + 2, y: 10},
-    { x: 0 + 10, y: 2},
-    { x: 0 + 20, y: 0 },
-  ] : [];
-
-  const borderTopRigth = borderTopLeftRight ? [
-    { x: width - 20, y: 0 }, //border right
-    { x: width - 10, y: 2 },
-    { x: width - 2, y: 10 },
-    { x: width, y: 20 },
-    { x: width, y: 0 },
-
-    { x: width, y: height }, // end left
-    { x: width + 10, y: height },
-    { x: width + 10, y: 0 },
-  ]: [];
-
-  const tab = shape
+  const curved = shape
     .line()
     .x((d) => d.x)
     .y((d) => d.y)
     .curve(shape.curveBasis)([
-    ...borderTopLeft,
 
-    { x: (width - circleWidth) / 2 - 20, y: 0 }, // border center left
-    { x: (width - circleWidth) / 2 - 10, y: 2 },
-    { x: (width - circleWidth) / 2 - 2, y: 10 },
-    { x: (width - circleWidth) / 2, y: 17 },
+      { x: (width - circleWidth) / 2 - 20, y: 0 }, // border center left
+      { x: (width - circleWidth) / 2 - 10, y: 2 },
+      { x: (width - circleWidth) / 2 - 2, y: 10 },
+      { x: (width - circleWidth) / 2, y: 17 },
 
-    {x: width / 2 - circleWidth / 2 + 8 , y: height / 2 + 2},
-    {x: width / 2 - 10, y: height / 2 + 10},
-    {x: width / 2, y: height / 2 + 10},
-    {x: width / 2 + 10, y: height / 2 + 10},
-    {x: width / 2 + circleWidth / 2 - 8, y: height / 2 + 2},
+      { x: width / 2 - circleWidth / 2 + 8, y: height / 2 + 2 },
+      { x: width / 2 - 10, y: height / 2 + 10 },
+      { x: width / 2, y: height / 2 + 10 },
+      { x: width / 2 + 10, y: height / 2 + 10 },
+      { x: width / 2 + circleWidth / 2 - 8, y: height / 2 + 2 },
 
-    { x: (width - circleWidth) / 2 + circleWidth, y: 17 }, // border center right
-    { x: (width - circleWidth) / 2 + circleWidth + 2, y: 10 },
-    { x: (width - circleWidth) / 2 + circleWidth + 10, y: 2 },
-    { x: (width - circleWidth) / 2 + circleWidth + 20, y: 0 },
-    
-     ...borderTopRigth
-  ]);
+      { x: (width - circleWidth) / 2 + circleWidth, y: 17 }, // border center right
+      { x: (width - circleWidth) / 2 + circleWidth + 2, y: 10 },
+      { x: (width - circleWidth) / 2 + circleWidth + 10, y: 2 },
+      { x: (width - circleWidth) / 2 + circleWidth + 20, y: 0 },
 
-  const right = shape
+    ], line);
+
+  const path = `${curved} ${line}`;
+
+  const pathBorderTopLeftRight = shape
     .line()
     .x((d) => d.x)
-    .y((d) => d.y)([
-    { x: width + circleWidth, y: 0 },
-    { x: width * 2, y: 0 },
-    { x: width * 2, y: height },
-    { x: 0, y: height },
-    { x: 0, y: 0 },
-  ]);
-  return `${left} ${tab} ${right}`;
+    .y((d) => d.y)
+    .curve(shape.curveBasis)([
+      // right
+      { x: (width - circleWidth) / 2 + circleWidth + 20, y: 0 },
+      { x: width - 20, y: 0 },
+      { x: width - 10, y: 2 },
+      { x: width - 2, y: 10 },
+      { x: width, y: 20 },
+      { x: width, y: height },
+      { x: width, y: height },
+      // bottom 
+      { x: width, y: height },
+      { x: 0, y: height },
+      // left
+      { x: 0, y: height },
+      { x: 0, y: height },
+      { x: 0, y: 20 },
+      { x: 0 + 2, y: 10 },
+      { x: 0 + 10, y: 2 },
+      { x: 0 + 20, y: 0 },
+      { x: (width - circleWidth) / 2 - 20, y: 0 },
+
+      { x: (width - circleWidth) / 2 - 20, y: 0 }, // border center left
+      { x: (width - circleWidth) / 2 - 10, y: 2 },
+      { x: (width - circleWidth) / 2 - 2, y: 10 },
+      { x: (width - circleWidth) / 2, y: 17 },
+
+      { x: width / 2 - circleWidth / 2 + 8, y: height / 2 + 2 },
+      { x: width / 2 - 10, y: height / 2 + 10 },
+      { x: width / 2, y: height / 2 + 10 },
+      { x: width / 2 + 10, y: height / 2 + 10 },
+      { x: width / 2 + circleWidth / 2 - 8, y: height / 2 + 2 },
+
+      { x: (width - circleWidth) / 2 + circleWidth, y: 17 }, // border center right
+      { x: (width - circleWidth) / 2 + circleWidth + 2, y: 10 },
+      { x: (width - circleWidth) / 2 + circleWidth + 10, y: 2 },
+      { x: (width - circleWidth) / 2 + circleWidth + 20, y: 0 },
+
+    ]);
+
+  if(borderTopLeftRight){
+    return pathBorderTopLeftRight;
+  }
+
+  return path;
 };
 
 
@@ -135,7 +154,7 @@ export const getPathUp = (width: number, height: number, circleWidth: number = 5
     .y((d) => d.y)([
       { x: 0, y: circleWidth - 5 },
       { x: 0, y: height },
-      {x:width, y: height},
+      { x: width, y: height },
       { x: width, y: circleWidth - 5 },
       { x: width - 20, y: 30 },
       { x: 20, y: 30 },
