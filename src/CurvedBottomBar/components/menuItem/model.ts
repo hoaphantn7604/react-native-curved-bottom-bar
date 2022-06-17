275b5c42b5eb388eac8d6ce9c29867e193306d4b
Hoa Phan <hoaphantn7604@gmail.com>
2022-06-18 00:33:18 +0700

    chore: update

--- src/CurvedBottomBar/components/menuItem/model.ts
+++ src/CurvedBottomBar/components/menuItem/model.ts
@@ -1,6 +1,8 @@
+import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 import type React from 'react';
 export interface ScreenBottomBarProps {
   name: string;
   position: 'LEFT' | 'RIGHT' | 'CENTER';
   component: (props: React.ComponentType<any>) => JSX.Element;
+  options?: BottomTabNavigationOptions;
 }
