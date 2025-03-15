ba50ee9b1bbbe9da468ad2d101e497b2650a9ae9
Dev Phan <hoaphantn7604@gmail.com>
2025-03-15 12:21:49 +0700

    feat: support new architecture

--- src/CurvedBottomBar/components/ShadowView/index.tsx
+++ src/CurvedBottomBar/components/ShadowView/index.tsx
@@ -1,29 +1,3 @@
-import { StyleProp } from 'react-native';
-import {
-  requireNativeComponent,
-  UIManager,
-  Platform,
-  ViewStyle,
-  View,
-} from 'react-native';
+import { View } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-curved-bottom-bar' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-type CurvedBottomBarProps = {
-  style?: StyleProp<ViewStyle>;
-};
-
-const ComponentName = 'CurvedBottomBarView';
-
-export const CurvedBottomBarView =
-  Platform.OS === 'android'
-    ? UIManager.getViewManagerConfig(ComponentName) != null
-      ? requireNativeComponent<CurvedBottomBarProps>(ComponentName)
-      : () => {
-          throw new Error(LINKING_ERROR);
-        }
-    : View;
+export const CurvedBottomBarView = View;
