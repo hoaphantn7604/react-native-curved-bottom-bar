9c5b93c031e09e858129da6b19898d5299eb2596
Dev Phan <hoaphantn7604@gmail.com>
2025-04-24 12:38:01 +0700

    fix: orientation

--- src/useDeviceOrientation.ts
+++ src/useDeviceOrientation.ts
@@ -1,37 +1,8 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-import { useEffect, useState } from 'react';
-import { Dimensions, ScaledSize } from 'react-native';
-
-const isOrientationPortrait = ({ width, height }: ScaledSize) =>
-  height >= width;
-const isOrientationLandscape = ({ width, height }: ScaledSize) =>
-  width >= height;
+import { useWindowDimensions } from 'react-native';
 
 export function useDeviceOrientation() {
-  const screen = Dimensions.get('screen');
-  const initialState = {
-    portrait: isOrientationPortrait(screen),
-    landscape: isOrientationLandscape(screen),
-  };
+  const { width, height } = useWindowDimensions();
 
-  const [orientation, setOrientation] = useState(initialState);
-
-  useEffect(() => {
-    const onChange = ({ screen }: { screen: ScaledSize }) => {
-      setOrientation({
-        portrait: isOrientationPortrait(screen),
-        landscape: isOrientationLandscape(screen),
-      });
-    };
-
-    const subscription = Dimensions.addEventListener('change', onChange);
-
-    return () => {
-      if (typeof subscription?.remove === 'function') {
-        subscription.remove();
-      }
-    };
-  }, []);
-
-  return orientation.portrait === true ? 'PORTRAIT' : 'LANDSCAPE';
+  return width < height ? 'PORTRAIT' : 'LANDSCAPE';
 }
